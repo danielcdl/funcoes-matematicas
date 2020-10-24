1d1d8c3b8d3fba6fcc9f78a77dfe2ec942d9214e
daniel chaves <danielchaves001@gmail.com>
2020-10-24 19:27:38 -0300

    criacao da funcao sac e funçao imprime_tabela_sac

--- amortizacao.py
+++ amortizacao.py
@@ -1,17 +1,92 @@
-def amortizar(p: float, n: int, i: float):
-    """
-    :param p: Valor da divida
-    :param n: Tempo em meses
-    :param i: Taxa de juros(composto) mensal
-    :return: Valor da parcela
-    """
+def imprimir_tabela_sac(dados,
+                        cabecalho=(
+                                'periodo',
+                                'saldo inicial',
+                                'juros calc.',
+                                'saldo após juros',
+                                'pgto', 'amortizado',
+                                'juros pago',
+                                'saldo final'
+                        ),
+                        lista_espacos=(1, 1, 1, 1, 1, 1, 1, 1)):
+                        #espacos=(9, 14, 12, 18, 6, 12, 12, 12)):
 
-    pgto = p / n + p * i
-    return pgto
+
+    # depois criar uma exceção caso cabeçalho e espaços tenham a quantidade de itens diferentes
+    espacos = []
+    for celula, espaco in zip(cabecalho, lista_espacos):
+        if len(celula) + 2 > espaco:
+            espacos.append(len(celula) + 2)
+        else:
+            espacos.append(espaco)
+
+    # cabeçalho
+    divisa_grossa = ' ' + '='*(sum(espacos) + len(espacos) + 1)
+    print(divisa_grossa)
+    print('||', end='')
+    for celula, espaco in zip(cabecalho, espacos):
+        escape = int((espaco - len(celula))/2) * ' '
+        print(escape + celula + escape, end='|')
+    print('|')
+    print(divisa_grossa)
+
+    # corpo
+    for linha in dados:
+        print('||', end='')
+        for celula, espaco in zip(linha, espacos):
+            dado = str(linha[celula])
+            escape = int((espaco - len(dado)) / 2) * ' '
+            print(escape + dado + escape, end='|')
+        print('|')
+
+    # rodapé
+    print(divisa_grossa)
 
 
-linha = [1, 500000.00, 7500.00, 5000.00, 2500.00, 45000.00]
-numero, saldo_inicial, prestacao, amortizacao, juros, saldo_final = linha
-print(
-    f'{numero:^6}         {saldo_inicial:>13,.2f}           {prestacao:>9,.2f}                {amortizacao:>11,.2f}           {juros:>9,.2f}        {saldo_final:>11,.2f}')
-# '  1        50,000.00    7,500.00       5,000.00     2500.00     45,000.00
+def sac(entradas=False):
+    n = 4
+    divida = 800
+    taxa = 0.1
+    if entradas:
+        while True:
+            try:
+                n = int(input('Digite a quantidade de meses: '))
+                break
+            except ValueError:
+                print('Erro! Digite um valor inteiro')
+
+        while True:
+            try:
+                divida = float(input('Digite o valor da divida: ').replace('.', '').replace(',', '.'))
+                break
+            except ValueError:
+                print('Erro! Digite um valor real')
+
+        while True:
+            try:
+                taxa = float(input('Digite o valor da taxa: ').replace('.', '').replace(',', '.'))
+                break
+            except ValueError:
+                print('Erro! Digite um valor real')
+
+    amortizado = divida / n
+    dados = []
+    for n in range(1, n + 1):
+        juros = divida * taxa
+        saldo_apos_juros = divida + juros
+        pgto = amortizado + juros
+        saldo_final = saldo_apos_juros - pgto
+        dados.append({'n': n,
+                      's_ini': divida,
+                      'juros': juros,
+                      's_apos_juros': saldo_apos_juros,
+                      'pgto': pgto,
+                      'amortizado': amortizado,
+                      'juros_pago': juros,
+                      'saldo_final': saldo_final
+                      })
+        divida = saldo_final
+
+    imprimir_tabela_sac(dados)
+
+sac()
