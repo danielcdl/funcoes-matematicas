12486a1534fa662904571ca8094492bfdf8bba49
daniel chaves <danielchaves001@gmail.com>
2020-10-24 21:23:04 -0300

    criação da função que escreve o número por extenso / criação da base de números para se escrever por extenso

---
+++ numero_por_extenso.py
@@ -0,0 +1,7 @@
+base_de_numeros = ('zero', 'um', 'dois','três','quatro','cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze',
+                   'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte', 'trinta',
+                   'quarenta', 'cincoeta', 'sesenta', 'setenta', 'oitenta', 'noventa', 'cem', 'duzentos', 'trezentos',
+                   'quatrocentos', 'quinhentos', 'seiscentos', 'setecentos', 'oitocentos', 'novecentos', 'mil','milhão',
+                   'milhões'
+                   )
+
