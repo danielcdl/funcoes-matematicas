cad91524695f4b92d963d928e11789f86fdf171d
daniel chaves <danielchaves001@gmail.com>
2020-10-25 04:46:44 -0300

    Nova implementação. Agora escreve até 999

--- numero_por_extenso.py
+++ numero_por_extenso.py
@@ -1,11 +1,11 @@
-base = {1:('zero', 'um', 'dois','três','quatro','cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze',
-                    'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove'),
-        2:'vinte', 3:'trinta',4:'quarenta', 5:'cincoeta', 6:'sesenta', 7:'setenta', 8:'oitenta', 9:'noventa',
-        'centena': ('cem', 'duzentos', 'trezentos', 'quatrocentos', 'quinhentos', 'seiscentos', 'setecentos',
-                   'oitocentos', 'novecentos'),
-        'unidade_milhar':('mil',),
+base = {1: ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze',
+            'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove'),
+        2: 'vinte', 3: 'trinta', 4: 'quarenta', 5: 'cincoeta', 6: 'sesenta', 7: 'setenta', 8: 'oitenta', 9: 'noventa',
+        'centena': ('cem', 'cento', 'duzentos', 'trezentos', 'quatrocentos', 'quinhentos', 'seiscentos', 'setecentos',
+                    'oitocentos', 'novecentos'),
+        'unidade_milhar': ('mil',),
         'milhar': ('milhão', 'milhões', 'bilhão', 'bilhões')
-       }
+        }
 
 
 def numero_por_extenso(numero):
@@ -15,28 +15,38 @@ def numero_por_extenso(numero):
         digitos[i] = int(digitos[i])
 
     n_extenso = ''
-    for digito, i in zip(digitos, range(tamanho+1)):
-        if i == tamanho-2:
+    for digito, i in zip(digitos, range(tamanho + 1)):
+        if i == tamanho - 3:
+            if digitos[i + 1:i + 3] == [0, 0]:
+                n_extenso += base['centena'][digito]
+                break
+            else:
+                n_extenso += base['centena'][digito] + ' e '
+        elif i == tamanho - 2:
             if digito < 2:
-                n_extenso += base[1][int(digitos[i]*10 + digitos[i+1])]
+                n_extenso += base[1][int(digitos[i] * 10 + digitos[i + 1])]
                 break
             else:
                 n_extenso += base[digito]
-                if digitos[i+1] != 0:
+                if digitos[i + 1] != 0:
                     n_extenso += ' e '
                 else:
                     break
-
-        elif i == tamanho-1:
+        elif i == tamanho - 1:
             n_extenso += base[1][digitos[i]]
 
-    print(n_extenso)
+    return n_extenso
 
-numero = 9
-while numero < 100:
-    try:
-        #numero = int(input('Digite um número inteiro: '))
-        numero_por_extenso(numero)
-        numero += 10
-    except ValueError:
-        print('Digite um número inteiro')
+
+def imprimir_por_extenso():
+    while True:
+        try:
+            numero = input('Digite um número inteiro: ')
+            print(numero_por_extenso(numero))
+            break
+        except ValueError:
+            print('ERRO!!', end='')
+
+
+if __name__ == '__main__':
+    imprimir_por_extenso()
